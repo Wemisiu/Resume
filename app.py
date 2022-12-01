b16c3246a879ff8b55d2b426001a32d91cc691a4
Daniel <wemisiu@icloud.com>
2022-12-01 09:08:03 -0600

    Change directory

---
+++ app.py
@@ -0,0 +1,17 @@
+#Import necessary packages and functions
+from flask import Flask, render_template
+
+# Create our web application
+app = Flask(__name__)
+
+# Create our routes
+@app.route('/')
+def home():
+	return render_template('index.html')
+
+# Start our application
+if __name__ == "__main__":
+	app.run(
+		debug=True,
+		port = 2888
+	)
