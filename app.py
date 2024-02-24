53f8ac597356e77ec41cd5ac1933f38846756692
Daniel <wemisiu@icloud.com>
2024-02-24 00:04:06 -0600

    Finishing up website

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 #Import necessary packages and functions
 from flask import Flask, render_template
 
+
+
 # Create our web application
 app = Flask(__name__)
+
+
 
 # Create our routes
 @app.route('/')
